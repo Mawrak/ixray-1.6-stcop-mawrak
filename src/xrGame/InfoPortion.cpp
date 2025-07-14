43affb03a55519bee209c55785236121a8b24593
St4lker0k765 <126080360+St4lker0k765@users.noreply.github.com>
2025-07-15 00:34:52 +0300

    Add basic SoC functionality

--- src/xrGame/InfoPortion.cpp
+++ src/xrGame/InfoPortion.cpp
@@ -1,7 +1,7 @@
 #include "StdAfx.h"
 #include "pch_script.h"
 #include "xml_str_id_loader.h"
-#include "object_broker.h"
+#include "../xrCore/object_broker.h"
 #include "InfoPortion.h"
 #include "GameObject.h"
 #include "encyclopedia_article.h"
@@ -107,14 +107,14 @@ void CInfoPortion::load_shared	(LPCSTR)
 		info_data()->m_ArticlesDisable.emplace_back(article_str_id);
 	}
 
-	/*info_data()->m_GameTasks.clear();
+	info_data()->m_GameTasks.clear();
 	const int task_num = pXML->GetNodesNum(pNode, "task");
 	for (int i = 0; i < task_num; ++i)
 	{
 		LPCSTR task_str_id = pXML->Read(pNode, "task", i, nullptr);
 		THROW(task_str_id);
 		info_data()->m_GameTasks.emplace_back(task_str_id);
-	}*/
+	}
 }
 
 void   CInfoPortion::InitXmlIdToIndex()
