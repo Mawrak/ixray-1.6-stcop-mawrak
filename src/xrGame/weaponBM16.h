95376eef73a2f73fe2d57b7352436e1a538cbfbd
St4lker0k765 <126080360+St4lker0k765@users.noreply.github.com>
2025-08-16 23:53:52 +0300

    Fix aim animations for BM-16

--- src/xrGame/weaponBM16.h
+++ src/xrGame/weaponBM16.h
@@ -22,6 +22,7 @@ protected:
 	virtual shared_str SetCurrentStateAnimation(const shared_str& first_name);
 	virtual shared_str SetCurrentShootAnimation();
 	virtual shared_str SetCurrentReloadAnimation();
+	virtual shared_str SetCurrentAimAnimation		();
 
 	virtual CWeaponBM16* cast_weapon_bm16() { return this; }
 
