8f8924e9d8f77596e35d360b0e98a8d7a1e9fe3f
St4lker0k765 <126080360+St4lker0k765@users.noreply.github.com>
2025-08-16 12:49:50 +0300

    Fix hints drawing

--- src/xrGame/ui/UIPdaWnd.cpp
+++ src/xrGame/ui/UIPdaWnd.cpp
@@ -530,6 +530,14 @@ void CUIPdaWnd::DrawHint()
 	{
 		pUITaskWnd->DrawHint();
 	}
+	if (m_sActiveSection == "eptQuests")
+	{
+		pUIEventsWnd->DrawHint();
+	}
+	else if (m_sActiveSection == "eptMap")
+	{
+		pUIMapWnd->DrawHint();
+	}
 	else if (m_sActiveSection == "eptFractionWar")
 	{
 		//m_hint_wnd->Draw();
