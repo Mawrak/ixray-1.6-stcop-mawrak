2e790a37216baa803dcb0e09bf68cc3d21730c5a
Mawrak <jenialuk@rambler.ru>
2025-10-01 01:03:58 +0300

    Restored debug console commands
    Счастье для всех, даром, и пусть никто не уйдёт обиженный

--- src/xrGame/console_commands.cpp
+++ src/xrGame/console_commands.cpp
@@ -2401,7 +2401,6 @@ void CCC_RegisterCommands()
 	g_OptConCom.Init();
 	CMD1(CCC_ChangeLanguage, "language");
 
-#ifndef MASTER_GOLD
 	CMD1(CCC_SetActorPosition, "set_actor_position");
 	CMD1(CCC_SetWeather, "set_weather");
 	CMD1(CCC_SetGameTime, "set_game_time");
@@ -2416,15 +2415,29 @@ void CCC_RegisterCommands()
 
 	CMD1(CCC_Particle_TEST, "g_ps_test");
 
-	CMD3(CCC_Mask, "dbg_draw_lchangers", &dbg_net_Draw_Flags, dbg_draw_lchangers);
 
 	CMD3(CCC_Mask, "g_unlimited_fire", &psActorFlags, AF_INFINITEFIRE);
 	CMD3(CCC_Mask, "g_unlimited_durability", &psActorFlags, AF_INFINITEDURABILITY);
 
+	
+    
+    
+    
+    
+    
+#ifndef MASTER_GOLD
+
+
+	CMD3(CCC_Mask, "dbg_draw_lchangers", &dbg_net_Draw_Flags, dbg_draw_lchangers);
+
 	CMD1(CCC_ChZLoggerTest, "chZLoggerTest");
 	CMD1(CCC_ConsoleColors, "get_console_colors");
 #endif
-
+    
+    
+    
+    
+    
 	CMD1(CCC_MemStats, "stat_memory");
 	// game
 	CMD3(CCC_Mask, "g_crouch_toggle", &psActorFlags, AF_CROUCH_TOGGLE);
@@ -2480,11 +2493,11 @@ void CCC_RegisterCommands()
 	//#endif // DEBUG
 
 		// Demo
-#if 1//ndef MASTER_GOLD
+
 	CMD1(CCC_DemoPlay, "demo_play");
 	CMD1(CCC_DemoRecord, "demo_record");
 	CMD1(CCC_DemoRecordSetPos, "demo_set_cam_position");
-#endif // #ifndef MASTER_GOLD
+
 
 #ifndef MASTER_GOLD
 	// ai
@@ -2597,14 +2610,14 @@ void CCC_RegisterCommands()
 	CMD3(CCC_Mask, "g_no_clip", &psActorFlags, AF_NO_CLIP);
 #endif // DEBUG
 
-#ifndef MASTER_GOLD
+
 	CMD1(CCC_JumpToLevel, "jump_to_level");
 	CMD3(CCC_Mask, "g_god", &psActorFlags, AF_GODMODE);
 	CMD3(CCC_Mask, "g_unlimitedammo", &psActorFlags, AF_UNLIMITEDAMMO);
 	CMD1(CCC_Script, "run_script");
 	CMD1(CCC_ScriptCommand, "run_string");
 	CMD1(CCC_TimeFactor, "time_factor");
-#endif // MASTER_GOLD
+
 
 	CMD1(CCC_ReloadSystemLtx, "reload_system_ltx");
 	CMD3(CCC_Mask, "g_autopickup", &psActorFlags, AF_AUTOPICKUP);
