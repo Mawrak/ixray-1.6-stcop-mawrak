9558c147bf266502a01f0669301b39f893e2d407
Shtrecker <gogels235@gmail.com>
2025-11-15 16:08:06 +0200

    Add alternate reload scheme for `CWeaponRG6` class

--- src/xrGame/WeaponRG6.h
+++ src/xrGame/WeaponRG6.h
@@ -10,6 +10,8 @@ class CWeaponRG6 final : public CRocketLauncher,
 	using inheritedRL = CRocketLauncher;
 	using inheritedSG = CWeaponShotgun;
 	
+	bool m_bAlternateReloadScheme = false;
+
 public:
 	CWeaponRG6() = default;
 	virtual ~CWeaponRG6() = default;
@@ -17,6 +19,10 @@ public:
 	virtual void	Load					(LPCSTR section) override;
 	virtual void	OnEvent					(NET_Packet& P, u16 type) override;
 	virtual CWeaponRG6* cast_weapon_rg6() { return this; }
+
+	virtual void	PlayAnimOpenWeapon() override;
+	virtual void	PlayAnimAddOneCartridgeWeapon() override;
+
 protected:
 	virtual u8		AddCartridge			(u8 cnt) override;
 	virtual void	UnloadMagazine			(bool spawn_ammo = true) override;
