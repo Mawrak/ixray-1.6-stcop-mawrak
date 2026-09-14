bb1bf04bb49848f7e333d57daf6227343815c371
St4lker0k765 <126080360+St4lker0k765@users.noreply.github.com>
2026-09-14 09:57:42 +0300

    Add custom animation for player 3D model

--- src/xrGame/ui/uiinventorywnd2.cpp
+++ src/xrGame/ui/uiinventorywnd2.cpp
@@ -54,9 +54,9 @@ void CUIInventoryWnd::InitInventory()
 	InventoryUtilities::UpdateWeight					(UIBagWnd, true);
 	if (m_pInvList[OUTFIT_SLOT])
 	{
-		if (CUIOutfitDragDropList* outfitSlot = smart_cast<CUIOutfitDragDropList*>(m_pInvList[OUTFIT_SLOT]))
+		if (CUIOutfitDragDropList* outfitSlot = m_pInvList[OUTFIT_SLOT]->ui_cast_outfit_dragdrop_list())
 		{
-			outfitSlot->SetOutfit();
+			outfitSlot->SetOutfit(outfitSlot->ItemsCount() != 0 ? outfitSlot->GetItemIdx(0) : nullptr);
 		}
 	}
 
